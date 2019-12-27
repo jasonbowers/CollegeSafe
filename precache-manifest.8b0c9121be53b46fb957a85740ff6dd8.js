c5f4953ee66a6c8699fe5e3b3384a99b719c4f70
Jason Bowers <flyvemaskine@gmail.com>
2019-12-26 21:25:43 -0800

    Updates

---
+++ precache-manifest.8b0c9121be53b46fb957a85740ff6dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71d78783da373e70fff6e627ae3be38",
+    "url": "/collegesafe/index.html"
+  },
+  {
+    "revision": "a321eefb57d4ece9b571",
+    "url": "/collegesafe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4f6e2ea9cd7cfe4c2b15",
+    "url": "/collegesafe/static/js/2.f58a1770.chunk.js"
+  },
+  {
+    "revision": "882265108c00bb7da64da6a7e04f8a40",
+    "url": "/collegesafe/static/js/2.f58a1770.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a321eefb57d4ece9b571",
+    "url": "/collegesafe/static/js/main.2ddb23b7.chunk.js"
+  },
+  {
+    "revision": "42d5b92cb9e5e8a0743d",
+    "url": "/collegesafe/static/js/runtime-main.ab1652ef.js"
+  }
+]);
